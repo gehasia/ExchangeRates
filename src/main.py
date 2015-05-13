e28f32085b5f8886e57107d97ac8886b7b30944a
Guillaume Chanaud <g.chanaud@regence-production.ch>
2015-05-13 17:25:33 +0200

    Add code for running outside GAE

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import webapp2, currency_rates, temperature, logging
+from google.appengine.api import apiproxy_stub_map
+from google.appengine.api.memcache import memcache_stub
+from google.appengine.api import urlfetch_stub
+apiproxy_stub_map.apiproxy.RegisterStub('memcache', memcache_stub.MemcacheServiceStub())
+apiproxy_stub_map.apiproxy.RegisterStub('urlfetch', urlfetch_stub.URLFetchServiceStub())  
 
 class Main(webapp2.RequestHandler):
     def get(self):
@@ -27,30 +32,23 @@ class Main(webapp2.RequestHandler):
         <div class="got-error">
             <i>Got an error?</i> Please help to create an issue at <a href="https://github.com/hippasus/ExchangeRates/issues">https://github.com/hippasus/ExchangeRates/issues</a>.
         </div>
-        <h3>Help needed!</h3>
-        <p>We've reached the <a href="https://developers.google.com/appengine/docs/quotas#UrlFetch" target="_blank">GAE URL Fetch Quota</a> for the currency API.
-        <br />And I need your help to enable billing to get larger quota. Do please help to click <a href="http://ko-fi.com?i=8d071856392d304" target="_blank">this link</a> and donate me $1.00USD.</p>
-        <p class="author">--By Hippasus Chu</p>
-
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-43673035-1', 'rate-exchange.appspot.com');
-            ga('send', 'pageview');
-        </script>
     </body>
 </html>
         """
         
         html = html.replace('$host_url', self.request.host_url)
-        
+     
         self.response.headers['Content-Type'] = 'text/html'
         self.response.out.write(html)
 
 app = webapp2.WSGIApplication([
                                ('/', Main),
                                ('/currency', currency_rates.CurrencyRates),
-                               ('/temperature', temperature.Temperature)])
+                               ('/temperature', temperature.Temperature)], debug=True)
+                               
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='0.0.0.0', port='80')
+        
+if __name__ == '__main__':
+    main()
